a9ec5ec5b6cd967e77f390f91542ad415d7e216c
keshavsoft <nknnkumar@yahoo.com>
2022-09-26 17:39:59 +0530

    keshav

--- Projects/JSONProject/Routes/AdminApi/Utility/Json/FromFolder/FromFile/ScreensFromDisplayJson/Items/Screens.js
+++ Projects/JSONProject/Routes/AdminApi/Utility/Json/FromFolder/FromFile/ScreensFromDisplayJson/Items/Screens.js
@@ -2,7 +2,9 @@ let express = require('express');
 let router = express.Router();
 
 let commonInsert = require("./Screens/Insert")
+let commonColumns = require("./Screens/Columns")
 
 router.use('/Insert', commonInsert);
+router.use('/Columns', commonColumns);
 
 module.exports = router;
