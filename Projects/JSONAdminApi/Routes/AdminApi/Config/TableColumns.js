e89b47446e4ae6499f1f5852c9bab11192794028
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2022-12-19 16:46:15 +0530

    SREENIVAS

--- Projects/JSONAdminApi/Routes/AdminApi/Config/TableColumns.js
+++ Projects/JSONAdminApi/Routes/AdminApi/Config/TableColumns.js
@@ -5,10 +5,12 @@ var router = express.Router();
 // router.use('/Update', require('./TableColumns/Update'));
 let CommonServer = require("./TableColumns/ServerSide")
 let CommonToggles = require("./TableColumns/Toggles")
+let CommonAllInOneWithValues = require("./TableColumns/AllInOneWithValues")
 
 router.use('/UpdateKeys', require('./TableColumns/UpdateKeys'));
 router.use('/ServerSide', CommonServer);
 router.use('/Toggles', CommonToggles);
+router.use('/AllInOneWithValues', CommonAllInOneWithValues);
 
 
 module.exports = router;
