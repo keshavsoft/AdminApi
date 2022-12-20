040d0e8c7d897c39152971cdd240710f05a92b67
JatinNeeraj <jatinucm123@gmail.com>
2022-12-20 18:48:54 +0530

    SREENIVAS

--- Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/SubKeys/ParentClasses.js
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/SubKeys/ParentClasses.js
@@ -1,8 +1,9 @@
-let CommonDataSupply = require("../../../../../../../../../../DataSupply");
+let CommonDataSupply = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/ItemName/ScreenName/TableColumns/SubMenu/ParentClasses/Update");
 
-exports.Update = async ({ inDataPk }) => {
-    let LocalinDataPk = inDataPk;
+exports.Update = async ({ DataPK, folderName, FileName, ItemName, ScreenName, DataAttribute, BodyAsJson }) => {
+    console.log("DAL-------------", DataPK, folderName, FileName, ItemName, ScreenName, DataAttribute, BodyAsJson);
+    let LocalinDataPk = DataPK;
     if (LocalinDataPk > 0) {
-        return await CommonDataSupply.AsArray({ inDataPk });
+        return await CommonDataSupply.Update({ DataPK, folderName, FileName, ItemName, ScreenName, DataAttribute, BodyAsJson });
     }
 };
