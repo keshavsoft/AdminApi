63d6efe8606cdea836b40e3f1573ac7926053cd8
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2022-12-26 21:11:15 +0530

    SREENIVAS

---
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/SubKeys/ServerSide.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+let CommonTransformBeforeSave = require("./ServerSide/TransformBeforeSave");
+
+router.use('/TransformBeforeSave', CommonTransformBeforeSave);
+
+module.exports = router;
