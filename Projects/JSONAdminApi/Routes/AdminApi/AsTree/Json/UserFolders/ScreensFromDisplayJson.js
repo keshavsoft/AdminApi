77e50cd551ad5451ffd07e1e2001247a15b376b4
JatinNeeraj <113779133+JatinNeeraj@users.noreply.github.com>
2023-01-02 20:27:41 +0530

    changes

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 let CommonTableColumns = require("./ScreensFromDisplayJson/TableColumns");
+let CommonTableInfo = require("./ScreensFromDisplayJson/TableInfo");
 
 router.use('/TableColumns', CommonTableColumns);
+router.use('/TableInfo', CommonTableInfo);
 
 module.exports = router;
