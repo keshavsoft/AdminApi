1cdbe0fb9f9f92e551322b02ae1b267b2674c413
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-01-04 17:12:13 +0530

    SREENIVAS

--- Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/FromKeys/ColumnReOrder.js
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/FromKeys/ColumnReOrder.js
@@ -1,5 +1,5 @@
 let CommonDatasupply = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/AsTree/ForTableInfo/FromKeys/ColumnReOrder");
-// let CommonDataSupplyForUpdate = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/ItemName/ScreenName/TableInfo/ColumnReOrder/Update")
+let CommonDataSupplyForUpdate = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/ItemName/ScreenName/TableInfo/FromKeys/ColumnReOrder/Update")
 
 let GetFuncs = async ({ DataPK }) => {
     if (DataPK > 0) {
--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 let CommonTableInfo = require("./TableInfo/FromKeys");
+let CommonToggles = require("./TableInfo/Toggles");
+
 router.use('/FromKeys', CommonTableInfo);
+router.use('/Toggles', CommonToggles);
 
 
 
