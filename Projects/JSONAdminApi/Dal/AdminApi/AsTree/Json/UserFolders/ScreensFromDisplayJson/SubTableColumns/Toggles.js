d7989fba5f725ff48376206a9c63812d69143e3d
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-01-11 16:12:42 +0530

    SREENIVAS

--- Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableColumns/Toggles.js
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableColumns/Toggles.js
@@ -1,5 +1,5 @@
 let CommonDatasupply = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/AsTree/ForSubTableColumns/Toggles");
-// let CommonDataSupplyForUpdate = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/ItemName/ScreenName/TableColumns/Toggles/Update")
+let CommonDataSupplyForUpdate = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/ItemName/ScreenName/TableColumns/Toggles/Update")
 
 let GetFuncs = async ({ DataPK }) => {
     console.log("oooooooooooooooooooo");
