4ad38e47998819949887f5bda2568ddb6e96d3e2
JatinNeeraj <113779133+JatinNeeraj@users.noreply.github.com>
2023-01-23 16:38:19 +0530

    jatin

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableColumns/TableInfo/FromKeys/ColumnReOrder.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableColumns/TableInfo/FromKeys/ColumnReOrder.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-let CommonControllers = require("../../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/FromKeys/ColumnReOrder.Controllers");
+let CommonControllers = require("../../../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableColumns/TableInfo/FromKeys/ColumnReOrder.Controllers");
 
 router.get('/', CommonControllers.GetFuncs);
 router.patch('/', CommonControllers.PatchFuncs);
--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableColumns/TableInfo/SubMenu/SearchRowArray/Button/PrintToPrinter.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableColumns/TableInfo/SubMenu/SearchRowArray/Button/PrintToPrinter.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-let CommonControllers = require("../../../../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/SearchRowArray/Button/PrintToPrinter");
+let CommonControllers = require("../../../../../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/SearchRowArray/Button/PrintToPrinter");
 
 router.get('/', CommonControllers.GetFuncs);
 router.patch('/', CommonControllers.PatchFuncs);
