06c46335278189945435130bf4fab13d07326745
JatinNeeraj <113779133+JatinNeeraj@users.noreply.github.com>
2023-02-06 20:25:02 +0530

    jatin

---
+++ Projects/JSONAdminApi/Repository/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromKeys/ItemNameConsider.js
@@ -0,0 +1,9 @@
+let Dal = require("../../../../../../../../Dal/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromKeys/Toggles");
+
+exports.GetFuncs = async ({ DataPK }) => {
+    return await Dal.GetFuncs({ DataPK });
+};
+
+exports.Update = async ({ DataPK,  ItemName, voucher, BodyAsJson }) => {
+    return await Dal.Update({  DataPK,  ItemName, voucher, BodyAsJson })
+};
