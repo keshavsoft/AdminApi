e09fdef128d1eda5761100e940045fd4d7fc7dcc
Keshav <nknnkumar@yahoo.com>
2023-02-09 19:47:59 +0530

    sreenivas

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile.js
@@ -3,8 +3,10 @@ let router = express.Router();
 
 let CommonFromKeys = require("./LedgerAutoJsonFile/FromKeys");
 let CommonAsTable = require("./LedgerAutoJsonFile/AsTable");
+let CommonVouchersConsider = require("./LedgerAutoJsonFile/VouchersConsider");
 
 router.use("/FromKeys", CommonFromKeys)
 router.use("/AsTable", CommonAsTable)
+router.use("/VouchersConsider", CommonVouchersConsider)
 
 module.exports = router;
