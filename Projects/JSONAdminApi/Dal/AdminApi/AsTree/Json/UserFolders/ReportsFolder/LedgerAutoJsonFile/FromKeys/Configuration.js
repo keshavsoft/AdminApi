24888edbb38ac3a6365d0ca4a2239009b2f60fa5
JatinNeeraj <113779133+JatinNeeraj@users.noreply.github.com>
2023-02-09 16:35:38 +0530

    jatin

---
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromKeys/Configuration.js
@@ -0,0 +1,20 @@
+let CommonDatasupply = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ReportsFolder/LedgerAutoJsonFile/AsTree/FromKeys/Configuration");
+let CommonDataSupplyForUpdate = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ReportsFolder/LedgerAutoJsonFile/InsideReport/FromKeys/Configuration/Update");
+
+let GetFuncs = async ({ DataPK }) => {
+    if (DataPK > 0) {
+        return await CommonDatasupply.StartFunc({ inDataPK: DataPK });
+    };
+};
+
+let Update = async ({  DataPK,  ItemName, voucher, BodyAsJson }) => {
+    let LocalDataPk = DataPK;
+
+    if (LocalDataPk > 0) {
+        return await CommonDataSupplyForUpdate.Update({ DataPK,  ItemName, voucher, BodyAsJson })
+    };
+};
+
+module.exports = {
+    GetFuncs, Update
+};
--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromKeys.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromKeys.js
@@ -3,8 +3,11 @@ let router = express.Router();
 
 let CommonToggles = require("./FromKeys/Toggles");
 let CommonColumnNameToPick = require("./FromKeys/ColumnNameToPick");
+let CommonConfiguration = require("./FromKeys/Configuration");
+
 
 router.use("/Toggles", CommonToggles)
 router.use("/ColumnNameToPick", CommonColumnNameToPick)
+router.use("/Configuration", CommonConfiguration)
 
 module.exports = router;
