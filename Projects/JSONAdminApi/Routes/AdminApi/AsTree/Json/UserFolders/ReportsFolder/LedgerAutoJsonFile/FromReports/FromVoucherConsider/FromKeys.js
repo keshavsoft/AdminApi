8d83b86ef843543ffad4ec06002a2c93bc71af85
JatinNeeraj <113779133+JatinNeeraj@users.noreply.github.com>
2023-02-15 20:39:39 +0530

    neeraj

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromReports/FromVoucherConsider/FromKeys.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromReports/FromVoucherConsider/FromKeys.js
@@ -4,10 +4,14 @@ let router = express.Router();
 let CommonToggles = require("./FromKeys/Toggles");
 let CommonColumnNameToPick = require("./FromKeys/ColumnNameToPick");
 let CommonConfiguration = require("./FromKeys/Configuration");
+let CommonFromFolder = require("./FromKeys/FromFolder");
+let CommonItemNameConsider= require("./FromKeys/ItemNameConsider");
 
 
 router.use("/Toggles", CommonToggles)
 router.use("/ColumnNameToPick", CommonColumnNameToPick)
 router.use("/Configuration", CommonConfiguration)
+router.use("/FromFolder", CommonFromFolder)
+router.use("/ItemNameConsider", CommonItemNameConsider)
 
 module.exports = router;
