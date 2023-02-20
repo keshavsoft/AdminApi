672500831e17aa56eefa3c6b819c3b91e3cfc8b7
JatinNeeraj <113779133+JatinNeeraj@users.noreply.github.com>
2023-02-20 20:20:50 +0530

    jatin

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromReports/FromVoucherConsider.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromReports/FromVoucherConsider.js
@@ -2,8 +2,10 @@ let express = require("express");
 let router = express.Router();
 
 let CommonFromKeys = require("./FromVoucherConsider/FromKeys");
+let CommonDuplicate = require("./FromVoucherConsider/Duplicate");
 
 router.use("/FromKeys", CommonFromKeys)
+router.use("/Duplicate", CommonDuplicate)
 
 
 module.exports = router;
