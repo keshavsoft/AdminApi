e37d84b29c0675861ff5882c020e08cb2968a44f
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-03-03 12:23:33 +0530

    SREENIVAS

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromReports/FromVoucherConsider/FromJoinTablesColumns.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromReports/FromVoucherConsider/FromJoinTablesColumns.js
@@ -3,11 +3,13 @@ let router = express.Router();
 
 let CommonDisplayColumn = require("./FromJoinTablesColumns/DisplayColumn");
 let CommonDuplicate = require("./FromJoinTablesColumns/Duplicate");
+let CommonJointConditions = require("./FromJoinTablesColumns/JointConditions");
 
 
 
 router.use("/DisplayColumn", CommonDisplayColumn)
 router.use("/Duplicate", CommonDuplicate)
+router.use("/JointConditions", CommonJointConditions)
 
 
 module.exports = router;
