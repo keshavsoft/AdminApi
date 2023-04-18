4d909af23e10b55ab82b1bfc446b8d3c073c4fdb
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-04-18 15:36:10 +0530

    SREENIVAS

---
+++ Projects/JSONAdminApi/Controllers/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/Duplicate/DuplicateFolder.Controllers.js
@@ -0,0 +1,35 @@
+let Repos = require("../../../../../../../../Repository/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/Duplicate/DuplicateFolder");
+
+let GetFuncs = async (req, res) => {
+    let LocalDataPk = req.KeshavSoft.DataPk;
+
+    let LocalFromRepo = await Repos.GetFuncs({
+        DataPK: LocalDataPk
+    });
+    res.json(LocalFromRepo)
+};
+
+
+let PatchFuncs = async (req, res) => {
+    let LocalDataPk = req.KeshavSoft.DataPk;
+    console.log("req.body-------------", req.body);
+
+    let jVarLocalfolderName = req.body.folderName;
+    let jVarLocalinToFolderName = req.body.inToFolderName;
+
+    let LocalFromRepo = await Repos.Clone({
+        inDataPK: LocalDataPk,
+        inFolderName: jVarLocalfolderName,
+        inToFolderName: jVarLocalinToFolderName
+    });
+    // console.log("LocalFromRepo", LocalFromRepo);
+
+    if (LocalFromRepo.KTF) {
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(204);
+    };
+};
+
+
+module.exports = { GetFuncs, PatchFuncs };
