570e916e71e06bb1f0481585ea335aebd8531dd7
JatinNeeraj <jatinucm123@gmail.com>
2023-04-24 18:10:29 +0530

    neeraj

---
+++ Projects/JSONAdminApi/Repository/AdminApi/AsTree/Json/UserFolders/ConfigFolder/UserFileAsFolder/JsonItem/Duplicate/DuplicateFile.js
@@ -0,0 +1,9 @@
+let Dal = require("../../../../../../../../../Dal/AdminApi/AsTree/Json/UserFolders/ConfigFolder/UserFileAsFolder/JsonItem/Duplicate/DuplicateFile");
+
+exports.GetFuncs = async ({ DataPK }) => {
+    return await Dal.GetFuncs({ DataPK });
+};
+
+exports.Clone = async ({ inFolderName, inFileNameOnly, inToFileName, inDataPK }) => {
+    return await Dal.Clone({ inFolderName, inFileNameOnly, inToFileName, inDataPK })
+};
