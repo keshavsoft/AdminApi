4e2599ef768cba7754b03a801154acc68df7fe98
JatinNeeraj <jatinucm123@gmail.com>
2023-04-25 19:44:52 +0530

    Jatin123

--- Projects/JSONAdminApi/Middlewares/ForRoutes/AdminApi/AsTree/Json/UserFolders/ConfigFolder/UserFileAsFolder/Duplicate/DuplicateFile.js
+++ Projects/JSONAdminApi/Middlewares/ForRoutes/AdminApi/AsTree/Json/UserFolders/ConfigFolder/UserFileAsFolder/Duplicate/DuplicateFile.js
@@ -4,6 +4,45 @@ exports.patchFunc = (req, res, next) => {
             KTF: false,
             KReason: "post requst body should contain : ",
             body: {
+                folderName: "",
+                inToFileName: "",
+                inFileNameOnly: ""
+            }
+        });
+
+        return;
+    };
+
+    if (("KeshavSoft" in req) === false) {
+        res.json({ KTF: false, KReason: "KeshavSoft not found in Request" })
+        return;
+    };
+    if (("DataPk" in req.KeshavSoft) === false) {
+        res.json({ KTF: false, KReason: "DataPk not found in KeshavSoft" })
+        return;
+    };
+    if (("folderName" in req.body) === false) {
+        res.json({ KTF: false, KReason: "folderName not found in body" })
+        return;
+    };
+    if (("inToFileName" in req.body) === false) {
+        res.json({ KTF: false, KReason: "inToFileName not found in body" })
+        return;
+    };
+    if (("inFileNameOnly" in req.body) === false) {
+        res.json({ KTF: false, KReason: "inFileNameOnly not found in body" })
+        return;
+    };
+
+    next();
+};
+
+exports.patchFunc1 = (req, res, next) => {
+    if (Object.keys(req.body).length === 0) {
+        res.json({
+            KTF: false,
+            KReason: "post requst body should contain : ",
+            body: {
                 folderName: ""
             }
         });
@@ -20,20 +59,14 @@ exports.patchFunc = (req, res, next) => {
                     res.json({ KTF: false, KReason: "folderName not found in body" })
 
                 } else {
-                    if (("inToFileName" in req.body) === false) {
-                        res.json({ KTF: false, KReason: "inToFileName not found in body" })
+                    if (("inToFolderName" in req.body) === false) {
+                        res.json({ KTF: false, KReason: "inToFolderName not found in body" })
 
                     } else {
-                        if (("inFileNameOnly" in req.body) === false) {
-                            res.json({ KTF: false, KReason: "inFileNameOnly not found in body" })
-
-                        } else {
-                            next();
-                        };
+                        next();
                     };
                 };
             };
         };
     };
 };
-
--- Projects/JSONAdminApi/Middlewares/ForRoutes/AdminApi/AsTree/Json/UserFolders/ConfigFolder/UserFileAsFolder/JsonItem/Duplicate/DuplicateItem.js
+++ Projects/JSONAdminApi/Middlewares/ForRoutes/AdminApi/AsTree/Json/UserFolders/ConfigFolder/UserFileAsFolder/JsonItem/Duplicate/DuplicateItem.js
@@ -10,6 +10,46 @@ exports.patchFunc = (req, res, next) => {
                 inToItemName:""
             }
         });
+
+        return;
+    };
+
+    if (("KeshavSoft" in req) === false) {
+        res.json({ KTF: false, KReason: "KeshavSoft not found in Request" })
+        return;
+    };
+    if (("DataPk" in req.KeshavSoft) === false) {
+        res.json({ KTF: false, KReason: "DataPk not found in KeshavSoft" })
+        return;
+    };
+    if (("folderName" in req.body) === false) {
+        res.json({ KTF: false, KReason: "folderName not found in body" })
+        return;
+    };
+    if (("FileName" in req.body) === false) {
+        res.json({ KTF: false, KReason: "FileName not found in body" })
+        return;
+    };
+    if (("ItemName" in req.body) === false) {
+        res.json({ KTF: false, KReason: "ItemName not found in body" })
+        return;
+    };
+    if (("inToItemName" in req.body) === false) {
+        res.json({ KTF: false, KReason: "inToItemName not found in body" })
+        return;
+    };
+    next();
+};
+
+exports.patchFunc1 = (req, res, next) => {
+    if (Object.keys(req.body).length === 0) {
+        res.json({
+            KTF: false,
+            KReason: "post requst body should contain : ",
+            body: {
+                folderName: ""
+            }
+        });
     } else {
         if (("KeshavSoft" in req) === false) {
             res.json({ KTF: false, KReason: "KeshavSoft not found in Request" })
@@ -23,25 +63,14 @@ exports.patchFunc = (req, res, next) => {
                     res.json({ KTF: false, KReason: "folderName not found in body" })
 
                 } else {
-                    if (("FileName" in req.body) === false) {
-                        res.json({ KTF: false, KReason: "FileName not found in body" })
+                    if (("inToFolderName" in req.body) === false) {
+                        res.json({ KTF: false, KReason: "inToFolderName not found in body" })
 
                     } else {
-                        if (("ItemName" in req.body) === false) {
-                            res.json({ KTF: false, KReason: "ItemName not found in body" })
-
-                        } else {
-                            if (("inToItemName" in req.body) === false) {
-                                res.json({ KTF: false, KReason: "inToItemName not found in body" })
-
-                            } else {
-                                next();
-                            };
-                        };
+                        next();
                     };
                 };
             };
         };
     };
 };
-
