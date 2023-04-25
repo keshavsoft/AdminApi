15378cbc0a0409d6018df8c9661d5610698f2729
Keshav <nknnkumar@yahoo.com>
2023-04-25 17:52:00 +0530

    keshav

--- Projects/JSONAdminApi/Middlewares/ForRoutes/AdminApi/AsTree/Json/UserFolders/ConfigFolder/Duplicate/DuplicateFolder.js
+++ Projects/JSONAdminApi/Middlewares/ForRoutes/AdminApi/AsTree/Json/UserFolders/ConfigFolder/Duplicate/DuplicateFolder.js
@@ -4,6 +4,40 @@ exports.patchFunc = (req, res, next) => {
             KTF: false,
             KReason: "post requst body should contain : ",
             body: {
+                folderName: "",
+                inToFolderName: ""
+            }
+        });
+
+        return;
+    };
+
+    if (("KeshavSoft" in req) === false) {
+        res.json({ KTF: false, KReason: "KeshavSoft not found in Request" })
+        return;
+    };
+    if (("DataPk" in req.KeshavSoft) === false) {
+        res.json({ KTF: false, KReason: "DataPk not found in KeshavSoft" })
+        return;
+    };
+    if (("folderName" in req.body) === false) {
+        res.json({ KTF: false, KReason: "folderName not found in body" })
+        return;
+    };
+    if (("inToFolderName" in req.body) === false) {
+        res.json({ KTF: false, KReason: "inToFolderName not found in body" })
+        return;
+    };
+
+    next();
+};
+
+exports.patchFunc1 = (req, res, next) => {
+    if (Object.keys(req.body).length === 0) {
+        res.json({
+            KTF: false,
+            KReason: "post requst body should contain : ",
+            body: {
                 folderName: ""
             }
         });
@@ -31,4 +65,3 @@ exports.patchFunc = (req, res, next) => {
         };
     };
 };
-
