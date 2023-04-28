dc95a42c435dfaea509b164a00cecea929fc4dc5
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-04-28 15:37:37 +0530

    SREENIVAS

---
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/DataFolder/Duplicate/DuplicateFolder.js
@@ -0,0 +1,20 @@
+let CommonDatasupply = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/AsTree/ForTableColumns/Duplicate/DuplicateFolder");
+let CommonDataSupplyForUpdate = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/UserFolder/Duplicate/DuplicateUserJsonFile");
+
+let GetFuncs = async ({ DataPK }) => {
+    if (DataPK > 0) {
+        return await CommonDatasupply.AsObject({ inDataPK: DataPK });
+    };
+};
+
+let Clone = async ({ inFolderName, inToFolderName, inDataPK }) => {
+    let LocalinDataPK = inDataPK;
+
+    if (LocalinDataPK > 0) {
+        return await CommonDataSupplyForUpdate.StartFunc({ inFolderName, inToFolderName, inDataPK:LocalinDataPK })
+    };
+};
+
+module.exports = {
+    GetFuncs, Clone
+};
