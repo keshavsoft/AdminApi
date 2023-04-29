02475e280257f03f5c69f92e7506384709b57e22
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-04-29 19:51:14 +0530

    SREENIVAS

--- Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/DataFolder/FileinFolder/Duplicate/DuplicateFile.js
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/DataFolder/FileinFolder/Duplicate/DuplicateFile.js
@@ -1,5 +1,5 @@
-let CommonDatasupply = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/AsTree/ForTableColumns/Duplicate/DuplicateFolder");
-let CommonDataSupplyForUpdate = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/Duplicate/DuplicateFileAsFolder");
+let CommonDatasupply = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/AsTree/UserFolder/UserFile/ForDuplicate");
+let CommonDataSupplyForUpdate = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/UserFolder/UserJsonFile/Duplicate/DuplicateUserFolder");
 
 let GetFuncs = async ({ DataPK }) => {
     if (DataPK > 0) {
