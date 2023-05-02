7b3730b207d568de4cfe01747eeac4c5105fc27c
JatinNeeraj <jatinucm123@gmail.com>
2023-05-02 20:30:42 +0530

    jatin

--- Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/DataFolder/FileinFolder/Duplicate/DuplicateFile.js
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/DataFolder/FileinFolder/Duplicate/DuplicateFile.js
@@ -1,9 +1,9 @@
-let CommonDatasupply = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/AsTree/ForTableColumns/Duplicate/DuplicateFolder");
-let CommonDataSupplyForUpdate = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/Duplicate/DuplicateFileAsFolder");
+let CommonDatasupply = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/AsTree/UserFolder/UserFile/ForDuplicate");
+let CommonDataSupplyForUpdate = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/UserFolder/UserJsonFile/Duplicate/DuplicateUserFolder");
 
 let GetFuncs = async ({ DataPK }) => {
     if (DataPK > 0) {
-        return await CommonDatasupply.AsObject({ inDataPK: DataPK });
+        return await CommonDatasupply.StartFunc({ inDataPK: DataPK });
     };
 };
 
@@ -11,7 +11,7 @@ let Clone = async ({ inFolderName, inFileNameOnly, inToFileName, inDataPK }) => 
     let LocalinDataPK = inDataPK;
 
     if (LocalinDataPK > 0) {
-        return await CommonDataSupplyForUpdate.StartFunc({ inFolderName, inFileNameOnly, inToFileName, inDataPK: LocalinDataPK })
+        return await CommonDataSupplyForUpdate.StartFunc({ inFolderName, inFileNameOnly, inToFileName, inDataPK: LocalinDataPK });
     };
 };
 
