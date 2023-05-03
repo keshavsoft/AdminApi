a35db12f7215bfb4380b1c9933b7b5fab7ec2ed6
JatinNeeraj <jatinucm123@gmail.com>
2023-05-03 12:31:40 +0530

    Jatin4155

---
+++ Projects/JSONAdminApi/Controllers/AdminApi/AsTree/Json/UserFolders/DataFolder/FileinFolder/JsonItem/Duplicate/DuplicateItem.Controllers.js
@@ -0,0 +1,37 @@
+let Repos = require("../../../../../../../../../Repository/AdminApi/AsTree/Json/UserFolders/DataFolder/FileinFolder/JsonItem/Duplicate/DuplicateItem");
+
+let GetFuncs = async (req, res) => {
+    let LocalDataPk = req.KeshavSoft.DataPk;
+
+    let LocalFromRepo = await Repos.GetFuncs({
+        DataPK: LocalDataPk
+    });
+    res.json(LocalFromRepo)
+};
+
+
+let PatchFuncs = async (req, res) => {
+    let LocalDataPk = req.KeshavSoft.DataPk;
+
+    let jVarLocalfolderName = req.body.folderName;
+    let jVarLocalFileName = req.body.FileName;
+    let jVarLocalItemName = req.body.ItemName;
+    let jVarLocalInToItemName = req.body.inToItemName;
+
+    let LocalFromRepo = await Repos.Clone({
+        inDataPK: LocalDataPk,
+        inFolderName: jVarLocalfolderName,
+        inFileNameOnly: jVarLocalFileName,
+        inItemName: jVarLocalItemName,
+        inToItemName: jVarLocalInToItemName
+    });
+
+    if (LocalFromRepo.KTF) {
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(204);
+    };
+};
+
+
+module.exports = { GetFuncs, PatchFuncs };
--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/DataFolder/FileinFolder.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/DataFolder/FileinFolder.js
@@ -2,7 +2,11 @@ let express = require("express");
 let router = express.Router();
 
 let CommonDuplicate = require("./FileinFolder/Duplicate");
+let CommonJsonItem = require("./FileinFolder/JsonItem");
+
 
 router.use("/Duplicate", CommonDuplicate)
+router.use("/JsonItem", CommonJsonItem)
+
 
 module.exports = router;
