84c6c023df877e8375e5c7a0071b5a9ce5370107
JatinNeeraj <jatinucm123@gmail.com>
2023-05-16 16:42:56 +0530

    jatin153

---
+++ Projects/JSONAdminApi/Middlewares/ForRoutes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/Toggles.js
@@ -0,0 +1,71 @@
+exports.GetFuncs = (req, res, next) => {
+    if (Object.keys(req.body).length === 0) {
+        res.json({
+            KTF: false,
+            KReason: "post requst body should contain : ",
+            body: {
+                folderName: "",
+                FileName: "",
+                ItemName: "",
+                ScreenName: "",
+                DataAttribute: "",
+                BodyAsJson: ""
+            }
+        });
+
+        return;
+    };
+
+    if (("KeshavSoft" in req) === false) {
+        res.json({ KTF: false, KReason: "KeshavSoft not found in Request" })
+        return;
+    };
+    if (("DataPk" in req.KeshavSoft) === false) {
+        res.json({ KTF: false, KReason: "DataPk not found in KeshavSoft" })
+        return;
+    };
+    if (("folderName" in req.body) === false) {
+        res.json({ KTF: false, KReason: "folderName not found in body" })
+        return;
+    };
+    if (("inToFolderName" in req.body) === false) {
+        res.json({ KTF: false, KReason: "inToFolderName not found in body" })
+        return;
+    };
+
+    next();
+};
+
+exports.patchFunc1 = (req, res, next) => {
+    if (Object.keys(req.body).length === 0) {
+        res.json({
+            KTF: false,
+            KReason: "post requst body should contain : ",
+            body: {
+                folderName: ""
+            }
+        });
+    } else {
+        if (("KeshavSoft" in req) === false) {
+            res.json({ KTF: false, KReason: "KeshavSoft not found in Request" })
+
+        } else {
+            if (("DataPk" in req.KeshavSoft) === false) {
+                res.json({ KTF: false, KReason: "DataPk not found in KeshavSoft" })
+
+            } else {
+                if (("folderName" in req.body) === false) {
+                    res.json({ KTF: false, KReason: "folderName not found in body" })
+
+                } else {
+                    if (("inToFolderName" in req.body) === false) {
+                        res.json({ KTF: false, KReason: "inToFolderName not found in body" })
+
+                    } else {
+                        next();
+                    };
+                };
+            };
+        };
+    };
+};
--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/Toggles.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/Toggles.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 let CommonControllers = require("../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/Toggles.Controllers");
+let CommonMiddlewares = require("../../../../../../../Middlewares/ForRoutes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/Toggles");
 
-router.get('/', CommonControllers.GetFuncs);
+router.get('/',CommonMiddlewares.GetFuncs, CommonControllers.GetFuncs);
 router.patch('/', CommonControllers.PatchFuncs);
 
 module.exports = router;
