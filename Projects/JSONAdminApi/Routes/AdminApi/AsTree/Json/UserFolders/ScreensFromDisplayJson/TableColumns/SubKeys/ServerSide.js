e5918262221eda080ba47cc769b2c5c68724f785
JatinNeeraj <jatinucm123@gmail.com>
2023-05-16 18:59:03 +0530

    jatin007

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/SubKeys/ServerSide.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/SubKeys/ServerSide.js
@@ -4,10 +4,13 @@ let CommonTransformBeforeSave = require("./ServerSide/TransformBeforeSave");
 let CommonDefaultValueShow = require("./ServerSide/DefaultValueShow");
 let CommonDefaultValueCreate = require("./ServerSide/DefaultValueCreate");
 let CommonSaveCheck = require("./ServerSide/SaveCheck");
+let CommonEnterToServer = require("./ServerSide/EnterToServer");
 
 router.use('/TransformBeforeSave', CommonTransformBeforeSave);
 router.use('/DefaultValueShow', CommonDefaultValueShow);
 router.use('/DefaultValueCreate', CommonDefaultValueCreate);
 router.use('/SaveCheck', CommonSaveCheck);
+router.use('/EnterToServer', CommonEnterToServer);
+
 
 module.exports = router;
