fcd826b2806aeaeae211f7269946b5c4aac11f84
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-05-17 11:16:12 +0530

    ramp147

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/Toggles.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/Toggles.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 let CommonControllers = require("../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/Toggles.Controllers");
-let CommonMiddlewares = require("../../../../../../../Middlewares/ForRoutes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/Toggles");
+let CommonMiddlewares = require("../../../../../../../Middlewares/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/Toggles")
+router.get('/', CommonControllers.GetFuncs);
+router.patch('/', CommonMiddlewares.PatchFuncs, CommonControllers.PatchFuncs);
 
-router.get('/',CommonMiddlewares.GetFuncs, CommonControllers.GetFuncs);
-router.patch('/', CommonControllers.PatchFuncs);
+
 
 module.exports = router;
