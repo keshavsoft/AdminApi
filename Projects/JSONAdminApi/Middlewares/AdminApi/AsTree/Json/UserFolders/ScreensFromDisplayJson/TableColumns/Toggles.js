a179778fba9ac010da99e0169cc90ad12acac760
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-05-18 17:41:51 +0530

    SREENIVAS

--- Projects/JSONAdminApi/Middlewares/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/Toggles.js
+++ Projects/JSONAdminApi/Middlewares/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/Toggles.js
@@ -1,6 +1,4 @@
-
 exports.PatchFuncs = (req, res, next) => {
-    console.log("req.body", req.body);
     if (Object.keys(req.body).length === 0) {
         res.json({
             KTF: false,
@@ -11,33 +9,38 @@ exports.PatchFuncs = (req, res, next) => {
                 ItemName: "",
                 ScreenName: "",
                 DataAttribute: "",
-                BodyAsJson: { }
+                BodyAsJson: {}
             },
         });
         return;
     };
-    if (("folderName" in req.body)===false) {
-        res.json({KTF:false,KReason:"folderName not found in body"})
+    if (("folderName" in req.body) === false) {
+        res.json({ KTF: false, KReason: "folderName not found in body" })
         return;
     };
-    if ((" FileName" in req.body)===false) {
-        res.json({KTF:false,KReason:" FileName not found in body"})
+
+    if (("FileName" in req.body) === false) {
+        res.json({ KTF: false, KReason: " FileName not found in body" })
         return;
     };
-    if (("ItemName" in req.body)===false) {
-        res.json({KTF:false,KReason:"ItemName not found in body"})
+
+    if (("ItemName" in req.body) === false) {
+        res.json({ KTF: false, KReason: "ItemName not found in body" })
         return;
     };
-    if (("ScreenName" in req.body)===false) {
-        res.json({KTF:false,KReason:" ScreenName not found in body"})
+
+    if (("ScreenName" in req.body) === false) {
+        res.json({ KTF: false, KReason: " ScreenName not found in body" })
         return;
     };
-    if (("DataAttribute" in req.body)===false) {
-        res.json({KTF:false,KReason:"DataAttribute not found in body"})
+
+    if (("DataAttribute" in req.body) === false) {
+        res.json({ KTF: false, KReason: "DataAttribute not found in body" })
         return;
     };
-    if (("BodyAsJson" in req.body)===false) {
-        res.json({KTF:false,KReason:" BodyAsJson not found in body"})
+
+    if (("BodyAsJson" in req.body) === false) {
+        res.json({ KTF: false, KReason: " BodyAsJson not found in body" })
         return;
     };
     next();
