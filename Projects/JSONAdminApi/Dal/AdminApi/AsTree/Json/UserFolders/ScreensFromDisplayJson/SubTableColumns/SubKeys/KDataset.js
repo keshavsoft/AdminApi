3cd30825598a21ab0b77551001ba31bb2522e552
Keshav <nknnkumar@yahoo.com>
2023-05-19 16:34:07 +0530

    keshav

--- Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableColumns/SubKeys/KDataset.js
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableColumns/SubKeys/KDataset.js
@@ -1,5 +1,5 @@
 let CommonDataSupplyForGet = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/AsTree/ForSubTableColumns/SubMenu/KDataset");
-let CommonDataSupply = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/ItemName/ScreenName/SubTableColumns/SubMenu/KDataset\Update");
+let CommonDataSupply = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/ItemName/ScreenName/SubTableColumns/SubMenu/KDataset/Update");
 
 exports.GetFuncs = async ({ DataPK }) => {
     let LocalinDataPk = DataPK;
