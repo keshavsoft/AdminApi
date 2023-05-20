fdc3beada31778771a2ab8df84eb7ff0d193a95f
JatinNeeraj <jatinucm123@gmail.com>
2023-05-20 13:07:42 +0530

    JATIN76

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableColumns/SubKeys.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableColumns/SubKeys.js
@@ -6,6 +6,8 @@ let CommonWidths = require("./SubKeys/Widths");
 let CommonKDatasetStuff = require("./SubKeys/KDatasetStuff");
 let CommonServerSide = require("./SubKeys/ServerSide");
 let CommonKDataset = require("./SubKeys/KDataset");
+let CommonKDataAttributes = require("./SubKeys/KDataAttributes");
+let CommonHtmlAttributes = require("./SubKeys/HtmlAttributes");
 
 router.use('/ParentClasses', CommonParentClasses);
 router.use('/DisplayType', CommonDisplayType);
@@ -13,5 +15,7 @@ router.use('/Widths', CommonWidths);
 router.use('/KDatasetStuff', CommonKDatasetStuff);
 router.use('/ServerSide', CommonServerSide);
 router.use('/KDataset', CommonKDataset);
+router.use('/HtmlAttributes', CommonHtmlAttributes);
+router.use('/KDataAttributes', CommonKDataAttributes);
 
 module.exports = router;
