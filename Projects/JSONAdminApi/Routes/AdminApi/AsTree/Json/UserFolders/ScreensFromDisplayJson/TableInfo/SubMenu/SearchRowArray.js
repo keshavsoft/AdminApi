0ba5d41a11ff1fea8a377eaed528fe8b7525285d
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-05-24 16:49:18 +0530

    SREENIVAS

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/SearchRowArray.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/SearchRowArray.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 let CommonLabel = require("./SearchRowArray/Label");
+let CommonSearch = require("./SearchRowArray/Search");
 
 router.use('/Label', CommonLabel);
+router.use('/Search', CommonSearch);
 
 module.exports = router;
