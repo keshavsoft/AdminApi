eb6da12ef44bbbf01ba43ca572a18589409ef3f7
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-05-29 12:51:34 +0530

    SREEENIVAS

---
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo/SubMenu/SearchRowArray/Search.js
@@ -0,0 +1,21 @@
+
+let CommonDatasupply = require("../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/AsTree/ForTableInfo/SubMenu/SearchRowArray/Search");
+let CommonDataSupplyForUpdate = require("../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/ItemName/ScreenName/TableInfo/SubMenu/SearchRowArray/Search/Update")
+
+let GetFuncs = async ({ DataPK }) => {
+    if (DataPK > 0) {
+        return await CommonDatasupply.StartFunc({ inDataPK: DataPK });
+    };
+};
+
+let Update = async ({ DataPK, FolderName, FileName, ItemName, ScreenName, DataAttribute, BodyAsJson }) => {
+    let LocalDataPk = DataPK;
+
+    if (LocalDataPk > 0) {
+        return await CommonDataSupplyForUpdate.Update({ DataPK, FolderName, FileName, ItemName, ScreenName, DataAttribute, BodyAsJson })
+    };
+};
+
+module.exports = {
+    GetFuncs, Update
+};
---
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo/SubMenu/SearchRowArray.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+let CommonLabel = require("./SearchRowArray/Label");
+let CommonSearch = require("./SearchRowArray/Search");
+let CommonnButton = require("./SearchRowArray/Button");
+
+router.use('/Label', CommonLabel);
+router.use('/Search', CommonSearch);
+router.use('/Button', CommonnButton);
+
+module.exports = router;
