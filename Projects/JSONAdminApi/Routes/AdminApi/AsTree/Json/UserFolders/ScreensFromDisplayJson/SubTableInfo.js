70b2b5196b1005a2a0b578077d636a2410f9cd5f
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-05-29 16:06:35 +0530

    SREENIVAS

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 let CommonToggles = require("./SubTableInfo/Toggles");
+let CommonSubMenu = require("./SubTableInfo/SubMenu");
 
 
 router.use('/Toggles', CommonToggles);
+router.use('/SubMenu', CommonSubMenu);
 
 module.exports = router;
