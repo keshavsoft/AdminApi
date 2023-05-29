543f7e472392ca31a372db9e966b1cbf19abad60
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-05-29 14:38:30 +0530

    ramp253

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo/SubMenu/SearchRowArray/Search.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo/SubMenu/SearchRowArray/Search.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-let CommonControllers = require("../../../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/SearchRowArray/Search");
+let CommonControllers = require("../../../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo/SubMenu/SearchRowArray/Search.Controllers");
 
 router.get('/', CommonControllers.GetFuncs);
 router.patch('/', CommonControllers.PatchFuncs);
