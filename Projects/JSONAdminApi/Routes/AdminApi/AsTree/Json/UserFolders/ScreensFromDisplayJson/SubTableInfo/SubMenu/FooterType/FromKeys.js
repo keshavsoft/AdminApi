cb83c6b8ea3bf2811a92e7e0810aa747daefd967
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-05-30 19:28:20 +0530

    SREENIVAS

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo/SubMenu/FooterType/FromKeys.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo/SubMenu/FooterType/FromKeys.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-let CommonControllers = require("../../../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/FooterType/FromKeys");
+let CommonControllers = require("../../../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo/SubMenu/FooterType/FromKeys.Controllers");
 
 router.get('/', CommonControllers.GetFuncs);
 router.patch('/', CommonControllers.PatchFuncs);
