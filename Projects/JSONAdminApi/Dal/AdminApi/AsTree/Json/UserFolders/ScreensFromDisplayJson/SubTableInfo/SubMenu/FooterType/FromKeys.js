ea0b941ab9cfb42dc4c3bdcdf063fd48170e345b
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-05-30 19:28:20 +0530

    SREENIVAS

--- Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo/SubMenu/FooterType/FromKeys.js
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo/SubMenu/FooterType/FromKeys.js
@@ -1,18 +1,19 @@
 
-let CommonDatasupply = require("../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/AsTree/ForTableInfo/SubMenu/FooterType/FromKeys");
-let CommonDataSupplyForUpdate = require("../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/ItemName/ScreenName/TableInfo/SubMenu/FooterType/FromKeys/Update")
+let CommonDatasupply = require("../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/AsTree/ForSubTableInfo/SubMenu/FooterType/FromKeys");
+let CommonDataSupplyForUpdate = require("../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/ItemName/ScreenName/SubTableInfo/SubMenu/FooterType/FromKeys/Update")
 
-let GetFuncs = async ({ DataPK }) => {
-    if (DataPK > 0) {
-        return await CommonDatasupply.AsObject({ inDataPK: DataPK });
+let GetFuncs = async ({ inDataPK }) => {
+    let LocalinDataPK = inDataPK
+    if (LocalinDataPK > 0) {
+        return await CommonDatasupply.AsObject({ inDataPK: LocalinDataPK });
     };
 };
 
-let Update = async ({ DataPK, FolderName, FileName, ItemName, ScreenName, DataAttribute, BodyAsJson }) => {
-    let LocalDataPk = DataPK;
+let Update = async ({ inDataPK, inFolderName, inFileName, inItemName, inScreenName, GridName, BodyAsJson }) => {
+    let LocalDataPk = inDataPK;
 
     if (LocalDataPk > 0) {
-        return await CommonDataSupplyForUpdate.Update({ DataPK, FolderName, FileName, ItemName, ScreenName, DataAttribute, BodyAsJson })
+        return await CommonDataSupplyForUpdate.Update({ inDataPK: LocalDataPk, inFolderName, inFileName, inItemName, inScreenName, GridName, BodyAsJson })
     };
 };
 
