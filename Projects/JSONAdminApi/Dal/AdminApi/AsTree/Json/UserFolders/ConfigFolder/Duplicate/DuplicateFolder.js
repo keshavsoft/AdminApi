5b11d2f15f0342f735670718a0de29ab4a667c76
keshavsoft <nknnkumar@yahoo.com>
2023-05-31 23:57:13 +0530

    keshav

--- Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ConfigFolder/Duplicate/DuplicateFolder.js
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ConfigFolder/Duplicate/DuplicateFolder.js
@@ -1,5 +1,6 @@
 let CommonDatasupply = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/AsTree/ForTableColumns/Duplicate/DuplicateFolder");
 let CommonDataSupplyForUpdate = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/Duplicate/DuplicateUserFolder");
+let CommonDataSupplyInsert = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigAndData/UserFolder/CreateFolder");
 
 let GetFuncs = async ({ DataPK }) => {
     if (DataPK > 0) {
@@ -11,7 +12,8 @@ let Clone = async ({ inFolderName, inToFolderName, inDataPK }) => {
     let LocalinDataPK = inDataPK;
 
     if (LocalinDataPK > 0) {
-        return await CommonDataSupplyForUpdate.StartFunc({ inFolderName, inToFolderName, inDataPK:LocalinDataPK })
+         return await CommonDataSupplyForUpdate.StartFunc({ inFolderName, inToFolderName, inDataPK: LocalinDataPK })
+        //return await CommonDataSupplyInsert.StartFunc({ inFolderName: inToFolderName, inDataPK: LocalinDataPK });
     };
 };
 
