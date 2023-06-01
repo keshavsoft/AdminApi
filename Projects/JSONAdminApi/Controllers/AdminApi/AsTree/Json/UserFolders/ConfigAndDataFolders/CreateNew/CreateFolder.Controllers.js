0631041419f74e0447e1b81fbdc00e24c09e1063
keshavsoft <nknnkumar@yahoo.com>
2023-06-01 14:45:02 +0530

    keshav

--- Projects/JSONAdminApi/Controllers/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/CreateNew/CreateFolder.Controllers.js
+++ Projects/JSONAdminApi/Controllers/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/CreateNew/CreateFolder.Controllers.js
@@ -1,13 +1,14 @@
-let Repos = require("../../../../../../../Repository/AdminApi/AsTree/Json/UserFolders/ConfigFolder/Duplicate/DuplicateFolder");
+let Repos = require("../../../../../../../Repository/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/CreateNew/CreateFolder");
 
 let GetFuncs = async (req, res) => {
-    // let LocalDataPk = req.KeshavSoft.DataPk;
+    let LocalDataPk = req.KeshavSoft.DataPk;
 
-    // let LocalFromRepo = await Repos.GetFuncs({
-    //     DataPK: LocalDataPk
-    // });
-    // res.json(LocalFromRepo)
-    res.end("test")
+    let LocalFromRepo = await Repos.GetFuncs({
+        DataPK: LocalDataPk
+    });
+
+    res.json(LocalFromRepo)
+    //res.end("test")
 };
 
 
