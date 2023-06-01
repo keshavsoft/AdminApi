777fd2feddd7224ec89ad77f82f56816b1f58ab2
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-06-01 13:22:55 +0530

    ramp1250

---
+++ Projects/JSONAdminApi/Controllers/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/CreateNew/CreateFolder.Controllers.js
@@ -0,0 +1,16 @@
+let Repos = require("../../../../../../../Repository/AdminApi/AsTree/Json/UserFolders/ConfigFolder/Duplicate/DuplicateFolder");
+
+let GetFuncs = async (req, res) => {
+    // let LocalDataPk = req.KeshavSoft.DataPk;
+
+    // let LocalFromRepo = await Repos.GetFuncs({
+    //     DataPK: LocalDataPk
+    // });
+    // res.json(LocalFromRepo)
+    res.end("test")
+};
+
+
+
+
+module.exports = { GetFuncs };
