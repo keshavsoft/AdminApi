a5ef8b102bc6b928d3ecf2b3085c45fb3175de02
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-06-01 15:21:16 +0530

    ramp1459

---
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/CreateNew/CreateFolder.js
@@ -0,0 +1,11 @@
+
+let GetFuncs = async ({ DataPK }) => {
+    if (DataPK > 0) {
+       // return await CommonDatasupply.AsObject({ inDataPK: DataPK });
+       return await "form Dal"
+    };
+};
+
+
+
+module.exports = { GetFuncs };
