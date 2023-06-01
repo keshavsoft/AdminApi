8b6922af540936c29411b24c2c1c5d80ba4ea88b
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-06-01 12:45:08 +0530

    ramp14520

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders.js
@@ -2,7 +2,9 @@ let express = require("express");
 let router = express.Router();
 
 let CommonCreateNew = require("./ConfigAndDataFolders/CreateNew");
+let CommonCreateFile = require("./ConfigAndDataFolders/UserFile");
 
-router.use("/CreateNew", CommonCreateNew)
+router.use("/CreateNew", CommonCreateNew);
+router.use("/UserFile", CommonCreateFile)
 
 module.exports = router;
---
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/UserFile.js
@@ -0,0 +1,8 @@
+let express = require("express");
+let router = express.Router();
+
+let CommonCreateNew = require("./UserFile/CreateNew");
+
+router.use("/CreateNew", CommonCreateNew)
+
+module.exports = router;
