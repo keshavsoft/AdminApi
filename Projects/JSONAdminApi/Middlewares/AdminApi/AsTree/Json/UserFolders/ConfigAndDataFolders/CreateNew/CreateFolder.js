8caf32def30d7739d7d68f172e7789ad125dfce8
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-06-02 13:38:36 +0530

    ramp12

---
+++ Projects/JSONAdminApi/Middlewares/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/CreateNew/CreateFolder.js
@@ -0,0 +1,22 @@
+
+exports.PostFunc = (req, res, next) => {
+    if (("DataPk" in req.KeshavSoft) === false) {
+        res.json({ KTF: false, KReason: "DataPk not found in Request" })
+        return;
+    };
+    if (Object.keys(req.body).length === 0) {
+        res.json({
+            KTF: false,
+            KReason: "post requst body should contain : ",
+            body: {
+                NewFolderName: "" 
+            },
+        });
+        return;
+    };
+    if (("NewFolderName" in req.body) === false) {
+        res.json({ KTF: false, KReason: "NewFolderName not found in body" })
+        return;
+    };
+    next();
+};
--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/CreateNew/CreateFolder.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/CreateNew/CreateFolder.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 let CommonControllers = require("../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/CreateNew/CreateFolder.Controllers");
-
-router.post('/', CommonControllers.PostFunc);
+let CommonMiddlewares = require("../../../../../../../Middlewares/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/CreateNew/CreateFolder")
+router.post('/',CommonMiddlewares.PostFunc, CommonControllers.PostFunc);
 
 module.exports = router;
