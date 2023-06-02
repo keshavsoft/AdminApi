c7fdba3091c34429e2c21c354568a0aea0a0cc7d
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-06-02 15:43:20 +0530

    ramp1444

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/UserFile/CreateNew/CreateFile.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/UserFile/CreateNew/CreateFile.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 let CommonControllers = require("../../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/UserFile/CreateNew/CreateFile.Controllers");
-
-router.post('/', CommonControllers.PostFunc);
+let CommonMiddlewares = require("../../../../../../../../Middlewares/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/UserFile/CreateNew/CreateFile")
+router.post('/',CommonMiddlewares.PostFunc, CommonControllers.PostFunc);
 
 module.exports = router;
