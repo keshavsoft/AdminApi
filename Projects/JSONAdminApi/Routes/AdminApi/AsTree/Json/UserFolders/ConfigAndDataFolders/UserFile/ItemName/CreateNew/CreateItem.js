7d722a374ed5d166430f70fd96ec82f05ccb5622
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-03 19:48:46 +0530

    keshav

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/UserFile/ItemName/CreateNew/CreateItem.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/UserFile/ItemName/CreateNew/CreateItem.js
@@ -3,7 +3,7 @@ var router = express.Router();
 let CommonControllers = require("../../../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/UserFile/ItemName/CreateNew/CreateItem.Controllers");
 let CommonMiddlewares = require("../../../../../../../../../Middlewares/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/UserFile/ItemName/CreateNew/CreateItem");
 
-router.post('/',CommonMiddlewares.PostFunc, CommonControllers.PostFunc);
-router.delete('/',CommonMiddlewares.PostFunc, CommonControllers.PostFunc);
+router.post('/', CommonMiddlewares.PostFunc, CommonControllers.PostFunc);
+router.delete('/', CommonMiddlewares.PostFunc, CommonControllers.PostFunc);
 
 module.exports = router;
