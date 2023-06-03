7d40b7ded402b12e0b0aa180c1bb801e5d4703ea
JatinNeeraj <jatinucm123@gmail.com>
2023-06-03 17:56:34 +0530

    Neeraj

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/UserFile/ItemName/ScreenName/CreateNew/CreateScreen.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/UserFile/ItemName/ScreenName/CreateNew/CreateScreen.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 let CommonControllers = require("../../../../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/UserFile/ItemName/ScreenName/CreateNew/CreateScreen.Controllers");
+let CommonMiddlewares = require("../../../../../../../../../../Middlewares/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/UserFile/ItemName/ScreenName/CreateNew/CreateScreen");
 
-router.post('/', CommonControllers.PostFunc);
+router.post('/', CommonMiddlewares.PostFunc,CommonControllers.PostFunc);
 
 module.exports = router;
