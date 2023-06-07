7e6230d16666c461779986188317f46be3ed3b04
keshavsoft <nknnkumar@yahoo.com>
2023-06-07 13:39:42 +0530

    keshav

--- Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/CreateNew/CreateFolder.js
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ConfigAndDataFolders/CreateNew/CreateFolder.js
@@ -1,6 +1,8 @@
 let CommonDatasupply = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigAndData/UserFolder/CreateFolder");
 let CommonDatasupplyDeleteFolder = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigAndData/UserFolder/DeleteFolder");
-let CommonDatasupplyGetFunc = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigAndData/UserFolder/GetTree");
+//let CommonDatasupplyGetFunc = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigAndData/UserFolder/GetTree");
+// let CommonDatasupplyGetFunc = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/getDirectories");
+let CommonDatasupplyGetFunc = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/getDirectoriesWithCheckAndDelete");
 
 let PostFunc = async ({ DataPK, inFolderName }) => {
     let LocalFolderName = inFolderName;
@@ -28,9 +30,8 @@ let GetFunc = async ({ DataPK, inFolderName }) => {
     let LocalFolderName = inFolderName;
 
     if (DataPK > 0) {
-        return await CommonDatasupplyGetFunc.StartFunc({
-            inDataPK: DataPK,
-            inFolderName: LocalFolderName
+        return await CommonDatasupplyGetFunc.AsObject({
+            inDataPK: DataPK
         });
     };
 };
