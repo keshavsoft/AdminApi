d430a67b941dcd1ed5990b00c441f68995e40a5f
JatinNeeraj <jatinucm123@gmail.com>
2023-06-07 15:20:51 +0530

    neeraj

---
+++ Projects/JSONAdminApi/Controllers/AdminApi/AsTree/Json/UserFolders/DataFolder/CreateNew/CreateFolder.Controllers.js
@@ -0,0 +1,42 @@
+let Repos = require("../../../../../../../Repository/AdminApi/AsTree/Json/UserFolders/DataFolder/CreateNew/CreateFolder");
+
+let PostFunc = async (req, res) => {
+    let LocalDataPk = req.KeshavSoft.DataPk;
+    let LocalFolderName = req.body.NewFolderName;
+
+    let LocalFromRepo = await Repos.PostFunc({
+        DataPK: LocalDataPk,
+        inFolderName: LocalFolderName
+    });
+
+    res.json(LocalFromRepo);
+    //res.end("test")
+};
+
+let DeleteFunc = async (req, res) => {
+    let LocalDataPk = req.KeshavSoft.DataPk;
+    let LocalFolderName = req.body.DeleteFolderName;
+
+    let LocalFromRepo = await Repos.DeleteFunc({
+        DataPK: LocalDataPk,
+        inFolderName: LocalFolderName
+    });
+
+    res.json(LocalFromRepo);
+    //res.end("test")
+};
+
+let GetFunc = async (req, res) => {
+    let LocalDataPk = req.KeshavSoft.DataPk;
+
+    let LocalFromRepo = await Repos.GetFunc({
+        DataPK: LocalDataPk
+    });
+
+    res.json(LocalFromRepo);
+    //res.end("test")
+};
+
+
+
+module.exports = { PostFunc, DeleteFunc, GetFunc };
