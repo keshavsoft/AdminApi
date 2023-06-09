3bd132d823ccbd3ad3ce425c6d49db703029a29f
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-09 19:38:44 +0530

    keshav

--- Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/AdminFolder/PreloadJsonFile/CreateNew/InsertKey.js
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/AdminFolder/PreloadJsonFile/CreateNew/InsertKey.js
@@ -1,37 +1,39 @@
-let CommonDatasupply = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigAndData/UserFolder/CreateFolder");
-let CommonDatasupplyDeleteFolder = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigAndData/UserFolder/DeleteFolder");
-//let CommonDatasupplyGetFunc = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigAndData/UserFolder/GetTree");
-// let CommonDatasupplyGetFunc = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/getDirectories");
-let CommonDatasupplyGetFunc = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/getDirectoriesWithCheckAndDelete");
+// let CommonDatasupply = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigAndData/UserFolder/CreateFolder");
+// let CommonDatasupplyDeleteFolder = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigAndData/UserFolder/DeleteFolder");
+// let CommonDatasupplyGetFunc = require("../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/getDirectoriesWithCheckAndDelete");
 
 let PostFunc = async ({ DataPK, inFolderName }) => {
-    let LocalFolderName = inFolderName;
+    return await "From DAL";
+    // let LocalFolderName = inFolderName;
 
-    if (DataPK > 0) {
-        return await CommonDatasupply.StartFunc({
-            inDataPK: DataPK,
-            inFolderName: LocalFolderName
-        });
-    };
+    // if (DataPK > 0) {
+    //     return await CommonDatasupply.StartFunc({
+    //         inDataPK: DataPK,
+    //         inFolderName: LocalFolderName
+    //     });
+    // };
 };
-let DeleteFunc = async ({ DataPK, inFolderName }) => {
-    let LocalFolderName = inFolderName;
 
-    if (DataPK > 0) {
-        return await CommonDatasupplyDeleteFolder.StartFunc({
-            inDataPK: DataPK,
-            inFolderName: LocalFolderName
-        });
-    };
+let DeleteFunc = async ({ DataPK, inFolderName }) => {
+    return await "From DAL";
+    // let LocalFolderName = inFolderName;
+
+    // if (DataPK > 0) {
+    //     return await CommonDatasupplyDeleteFolder.StartFunc({
+    //         inDataPK: DataPK,
+    //         inFolderName: LocalFolderName
+    //     });
+    // };
 };
 
 let GetFunc = async ({ DataPK }) => {
+    return await "From DAL";
 
-    if (DataPK > 0) {
-        return await CommonDatasupplyGetFunc.AsObject({
-            inDataPK: DataPK
-        });
-    };
+    // if (DataPK > 0) {
+    //     return await CommonDatasupplyGetFunc.AsObject({
+    //         inDataPK: DataPK
+    //     });
+    // };
 };
 
 module.exports = {
