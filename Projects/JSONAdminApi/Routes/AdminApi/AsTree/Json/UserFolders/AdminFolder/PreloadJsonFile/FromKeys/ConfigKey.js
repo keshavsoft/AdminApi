66d60375037a82e186dc2493982923bebe117c1a
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-06-14 16:23:43 +0530

    ram14023

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/AdminFolder/PreloadJsonFile/FromKeys/ConfigKey.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/AdminFolder/PreloadJsonFile/FromKeys/ConfigKey.js
@@ -4,9 +4,11 @@ let router = express.Router();
 let CommonItemConfigKey = require("./ConfigKey/ItemConfigKey");
 let CommonJsonConfigKey = require("./ConfigKey/JsonConfigKey");
 let CommonColumnsKey = require("./ConfigKey/ColumnsKey");
+let CommonDataListInfoKey = require("./ConfigKey/DataListInfoKey");
 
 router.use("/ItemConfigKey", CommonItemConfigKey);
 router.use("/JsonConfigKey", CommonJsonConfigKey);
 router.use("/ColumnsKey", CommonColumnsKey);
+router.use("/DataListInfoKey", CommonDataListInfoKey);
 
 module.exports = router;
