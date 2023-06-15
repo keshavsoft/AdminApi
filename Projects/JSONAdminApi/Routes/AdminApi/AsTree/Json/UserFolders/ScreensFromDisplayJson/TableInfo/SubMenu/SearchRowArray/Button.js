456ba459153d159d4bad2df49b056be90c912487
JatinNeeraj <jatinucm123@gmail.com>
2023-06-15 18:38:12 +0530

    neeraj78

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/SearchRowArray/Button.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/SearchRowArray/Button.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 let CommonButton = require("./Button/NewWindow");
+let CommonPopUp = require("./Button/PopUp");
 
 router.use('/NewWindow', CommonButton);
+router.use('/PopUp', CommonPopUp);
 
 module.exports = router;
