cbcdad5114ad332eb952eeb7006718f018257d5c
JatinNeeraj <jatinucm123@gmail.com>
2023-06-22 17:51:45 +0530

    neraj65

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/TableRowOptions/Delete.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/TableRowOptions/Delete.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 let CommonControllers = require("../../../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/TableRowOptions/Delete.Controllers");
+let CommonMiddlewares = require("../../../../../../../../../Middlewares/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/TableRowOptions/Delete");
 
-router.get('/', CommonControllers.GetFuncs);
-router.patch('/', CommonControllers.PatchFuncs);
+router.get('/',CommonMiddlewares.GetFuncs, CommonControllers.GetFuncs);
+router.patch('/',CommonMiddlewares.PatchFuncs, CommonControllers.PatchFuncs);
 
 
 
