50224c6abfcb9fd210ad0127ff1b5fc6d9a8b998
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-06-22 17:47:38 +0530

    ram1035

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/TableRowOptions/Show.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/TableRowOptions/Show.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 let CommonControllers = require("../../../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/TableRowOptions/Show.Controllers");
+let CommonMiddlewares = require("../../../../../../../../../Middlewares/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableInfo/SubMenu/TableRowOptions/Show");
 
 router.get('/', CommonControllers.GetFuncs);
-router.patch('/', CommonControllers.PatchFuncs);
+router.patch('/',CommonMiddlewares.PatchFuncs, CommonControllers.PatchFuncs);
 
 
 
