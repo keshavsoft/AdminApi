504f0a8e4441f3d318563dbe13c4d75eeec097c1
JatinNeeraj <jatinucm123@gmail.com>
2023-06-22 16:25:20 +0530

    neeraj5

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo/SubMenu/TableRowOptions/Edit.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo/SubMenu/TableRowOptions/Edit.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 let CommonControllers = require("../../../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo/SubMenu/TableRowOptions/Edit.Controllers");
-let CommonMiddlewares = require("../../../../../../../../../Middlewares/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo/SubMenu/TableRowOptions/Delete");
+let CommonMiddlewares = require("../../../../../../../../../Middlewares/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableInfo/SubMenu/TableRowOptions/Edit");
 
 router.get('/', CommonControllers.GetFuncs);
 router.patch('/', CommonMiddlewares.PatchFuncs, CommonControllers.PatchFuncs);
