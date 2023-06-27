19e806b33ca8945a9f63cec0245d9e5307bfa2c5
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-06-27 12:32:40 +0530

    ramp25367

---
+++ Projects/JSONAdminApi/Controllers/AdminApi/AsTree/Json/UserFolders/DataFolder/FileinFolder/ItemName/CreateNew/CreateItemWithData.Controllers.js
@@ -0,0 +1,45 @@
+let Repos = require("../../../../../../../../../Repository/AdminApi/AsTree/Json/UserFolders/DataFolder/FileinFolder/ItemName/CreateNew/CreateItemWithData");
+
+let PostFunc = async (req, res) => {
+   let LocalDataPk = req.KeshavSoft.DataPk;
+   let LocalFolderName = req.body.FolderName;
+   let LocalFileName = req.body.FileName;
+   let LocalItemName = req.body.NewItemName;
+
+   let LocalFromRepo = await Repos.PostFunc({
+      DataPK: LocalDataPk,
+      inFolderName: LocalFolderName,
+      inFileName: LocalFileName,
+      inNewItemName: LocalItemName
+   });
+
+   res.json(LocalFromRepo);
+   //res.end("testItme");
+};
+let DeleteFunc = async (req, res) => {
+   let LocalDataPk = req.KeshavSoft.DataPk;
+   let LocalFolderName = req.body.FolderName;
+   let LocalFileName = req.body.FileName;
+   let LocalItemName = req.body.DeleteItemName;
+
+   let LocalFromRepo = await Repos.DeleteFunc({
+      DataPK: LocalDataPk,
+      inFolderName: LocalFolderName,
+      inFileName: LocalFileName,
+      inNewItemName: LocalItemName
+   });
+
+   res.json(LocalFromRepo);
+   //res.end("testItme");
+};
+let GetFunc = async (req, res) => {
+   let LocalDataPk = req.KeshavSoft.DataPk;
+
+   let LocalFromRepo = await Repos.GetFunc({
+      DataPK: LocalDataPk
+   });
+
+   res.json(LocalFromRepo);
+   //res.end("testItme");
+};
+module.exports = { PostFunc,DeleteFunc,GetFunc };
--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/DataFolder/FileinFolder/ItemName/CreateNew.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/DataFolder/FileinFolder/ItemName/CreateNew.js
@@ -2,7 +2,9 @@ let express = require("express");
 let router = express.Router();
 
 let CommonCreateItem = require("./CreateNew/CreateItem");
+let CommonCreateItemWithData = require("./CreateNew/CreateItemWithData");
 
 router.use("/CreateItem", CommonCreateItem);
+router.use("/CreateItemWithData", CommonCreateItemWithData);
 
 module.exports = router;
