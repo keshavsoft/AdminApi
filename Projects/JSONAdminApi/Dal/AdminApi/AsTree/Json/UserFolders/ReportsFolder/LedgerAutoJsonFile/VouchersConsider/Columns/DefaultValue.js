890221266bc93e821e58a341d18a2d522156efa1
JatinNeeraj <jatinucm123@gmail.com>
2023-07-04 13:45:36 +0530

    neeraj2

---
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/VouchersConsider/Columns/DefaultValue.js
@@ -0,0 +1,20 @@
+//let CommonDatasupply = require("../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ReportsFolder/LedgerAutoJsonFile/AsTree/FromVoucheConsider/Columns");
+//let CommonDataSupplyForUpdate = require("../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ReportsFolder/LedgerAutoJsonFile/InsideReport/VouchersConsider/InsideVouchersConsider/Columns/DisplayColumn/Update");
+let GetFuncs = async ({ DataPK }) => {
+    return"FromDal"
+    if (DataPK > 0) {
+        return await CommonDatasupply.StartFunc({ inDataPK: DataPK });
+    };
+};
+
+let Update = async ({ DataPK, ReportName, voucherconsiderpk, columnpk, BodyAsJson }) => {
+    let LocalDataPk = DataPK;
+
+    if (LocalDataPk > 0) {
+        return await CommonDataSupplyForUpdate.StartFunc({ inDataPK: LocalDataPk, ReportName, voucherconsiderpk, columnpk, BodyAsJson })
+    };
+};
+
+module.exports = {
+    GetFuncs, Update
+};
