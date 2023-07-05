e430544a0cf057bcd9c2125bbdf39d49ed6dc41b
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-07-05 18:08:56 +0530

    ram4576

---
+++ Projects/JSONAdminApi/Middlewares/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromReports/FromVoucherConsider/FromKeys/ItemNameConsider.js
@@ -0,0 +1,34 @@
+
+exports.PatchFuncs = (req, res, next) => {
+    if (("DataPk" in req.KeshavSoft) === false) {
+        res.json({ KTF: false, KReason: "DataPk not found in Request" })
+        return;
+    };
+
+    if (Object.keys(req.body).length === 0) {
+        res.json({
+            KTF: false,
+            KReason: "post requst body should contain : ",
+            body: {
+                ItemName: "",
+                voucher:"",
+                BodyAsJson:{}           
+            },
+        });
+        return;
+    };
+    
+    if (("ItemName" in req.body) === false) {
+        res.json({ KTF: false, KReason: "ItemName not found in body" })
+        return;
+    }; 
+    if (("voucher" in req.body) === false) {
+        res.json({ KTF: false, KReason: "voucher not found in body" })
+        return;
+    }; 
+    if (("BodyAsJson" in req.body) === false) {
+        res.json({ KTF: false, KReason: "BodyAsJson not found in body" })
+        return;
+    };   
+    next();
+};
