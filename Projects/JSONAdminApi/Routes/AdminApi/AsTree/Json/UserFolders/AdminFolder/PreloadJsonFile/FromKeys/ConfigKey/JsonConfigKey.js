68ecc1ac98e219085e8c07d14b5fa678b7f08edf
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-07-06 18:30:30 +0530

    ramp457120

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/AdminFolder/PreloadJsonFile/FromKeys/ConfigKey/JsonConfigKey.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/AdminFolder/PreloadJsonFile/FromKeys/ConfigKey/JsonConfigKey.js
@@ -3,7 +3,7 @@ var router = express.Router();
 let CommonControllers = require("../../../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/AdminFolder/PreloadJsonFile/FromKeys/ConfigKey/JsonConfigKey");
 let CommonMiddlewares = require("../../../../../../../../../Middlewares/AdminApi/AsTree/Json/UserFolders/AdminFolder/PreloadJsonFile/FromKeys/ConfigKey/JsonConfigKey");
 
-router.post('/', CommonControllers.PostFunc);
+router.post('/',CommonMiddlewares.PostFunc, CommonControllers.PostFunc);
 router.delete('/', CommonMiddlewares.DeleteFunc, CommonControllers.DeleteFunc);
 router.get('/', CommonControllers.GetFunc);
 
