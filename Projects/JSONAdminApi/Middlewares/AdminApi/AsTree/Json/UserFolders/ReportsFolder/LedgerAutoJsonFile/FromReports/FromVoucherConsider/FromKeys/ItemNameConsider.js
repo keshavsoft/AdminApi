a479158592212c9b0a5cb35039e4d89b20e9eb9b
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-07-10 13:52:43 +0530

    ramp1245

--- Projects/JSONAdminApi/Middlewares/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromReports/FromVoucherConsider/FromKeys/ItemNameConsider.js
+++ Projects/JSONAdminApi/Middlewares/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromReports/FromVoucherConsider/FromKeys/ItemNameConsider.js
@@ -10,9 +10,14 @@ exports.PatchFuncs = (req, res, next) => {
             KTF: false,
             KReason: "post requst body should contain : ",
             body: {
-                ItemName: "",
-                voucher:"",
-                BodyAsJson:{}           
+                ReportName: "",
+                VoucherPk:"",
+                BodyAsJson:{
+                    FolderName:"",
+                    FileName:"",
+                    ItemName:"",
+                    ItemNameConsider:""
+                }           
             },
         });
         return;
@@ -29,7 +34,22 @@ exports.PatchFuncs = (req, res, next) => {
     if (("BodyAsJson" in req.body) === false) {
         res.json({ KTF: false, KReason: "BodyAsJson not found in body" })
         return;
-    };  
+    };
+    if (("FolderName" in req.body.BodyAsJson) === false) {
+        res.json({ KTF: false, KReason: "FolderName not found in BodyAsJson body" })
+        return;
+    };
+    if (("FileName" in req.body.BodyAsJson) === false) {
+        res.json({ KTF: false, KReason: "FileName not found in BodyAsJson body" })
+        return;
+    };
+    if (("ItemName" in req.body.BodyAsJson) === false) {
+        res.json({ KTF: false, KReason: "ItemName not found in BodyAsJson body" })
+        return;
+    }; if (("ItemNameConsider" in req.body.BodyAsJson) === false) {
+        res.json({ KTF: false, KReason: "ItemNameConsider not found in BodyAsJson body" })
+        return;
+    };   
     next();
 };
 
@@ -44,9 +64,14 @@ exports.PostFuncs = (req, res, next) => {
             KTF: false,
             KReason: "post requst body should contain : ",
             body: {
-                ItemName: "",
-                voucher:"",
-                BodyAsJson:{}           
+                ReportName: "",
+                VoucherPk:"",
+                BodyAsJson:{
+                    FolderName:"",
+                    FileName:"",
+                    ItemName:"",
+                    ItemNameConsider:""
+                }           
             },
         });
         return;
@@ -63,6 +88,21 @@ exports.PostFuncs = (req, res, next) => {
     if (("BodyAsJson" in req.body) === false) {
         res.json({ KTF: false, KReason: "BodyAsJson not found in body" })
         return;
-    };   
+    };
+    if (("FolderName" in req.body.BodyAsJson) === false) {
+        res.json({ KTF: false, KReason: "FolderName not found in BodyAsJson body" })
+        return;
+    };
+    if (("FileName" in req.body.BodyAsJson) === false) {
+        res.json({ KTF: false, KReason: "FileName not found in BodyAsJson body" })
+        return;
+    };
+    if (("ItemName" in req.body.BodyAsJson) === false) {
+        res.json({ KTF: false, KReason: "ItemName not found in BodyAsJson body" })
+        return;
+    }; if (("ItemNameConsider" in req.body.BodyAsJson) === false) {
+        res.json({ KTF: false, KReason: "ItemNameConsider not found in BodyAsJson body" })
+        return;
+    };  
     next();
 };
