c9e70a720c89cb9c3734ce99f14c4858ce6d5195
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-07-13 20:20:09 +0530

    SREENIVAS

--- Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromReports/FromVoucherConsider/CreateNew.js
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromReports/FromVoucherConsider/CreateNew.js
@@ -1,9 +1,11 @@
 let CommonDatasupply = require("../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ReportsFolder/LedgerAutoJsonFile/AsTree/FromVoucheConsider/Duplicate");
 let CommonDataSupplyForCreate = require("../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ReportsFolder/LedgerAutoJsonFile/InsideReport/VouchersConsider/CreateNew/CreateNew");
 
-let GetFuncs = async ({ DataPK }) => {
-    if (DataPK > 0) {
-        return await CommonDatasupply.StartFunc({ inDataPK: DataPK });
+let GetFuncs = async ({ inDataPK }) => {
+    let LocalDataPk = inDataPK;
+
+    if (LocalDataPk > 0) {
+        return await CommonDatasupply.StartFunc({ inDataPK: LocalDataPk });
     };
 };
 
