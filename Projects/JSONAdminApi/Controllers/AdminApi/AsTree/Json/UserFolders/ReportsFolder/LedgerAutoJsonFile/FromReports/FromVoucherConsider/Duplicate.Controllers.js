7269cfd754bc7317583a2a46ba45007ffe487dae
Satish Addanki <satishaddanki321@gmail.com>
2023-07-21 15:45:07 +0530

    satish

--- Projects/JSONAdminApi/Controllers/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromReports/FromVoucherConsider/Duplicate.Controllers.js
+++ Projects/JSONAdminApi/Controllers/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromReports/FromVoucherConsider/Duplicate.Controllers.js
@@ -21,12 +21,12 @@ let PatchFuncs = async (req, res) => {
         ReportName: jVarLocalReportname,
         inVoucherConsiderpk: jVarLocalVouchersConsiderPK
     });
-
-    if (LocalFromRepo.KTF) {
-        res.sendStatus(200);
-    } else {
-        res.sendStatus(204);
-    };
+    res.json(LocalFromRepo);
+    // if (LocalFromRepo.KTF) {
+    //     res.sendStatus(200);
+    // } else {
+    //     res.sendStatus(204);
+    // };
 };
 
 
