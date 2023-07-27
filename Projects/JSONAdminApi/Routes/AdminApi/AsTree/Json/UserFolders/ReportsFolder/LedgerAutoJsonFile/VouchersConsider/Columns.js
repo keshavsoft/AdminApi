9d6f15d9a3fec53354e6b67368fa563551866598
JatinNeeraj <jatinucm123@gmail.com>
2023-07-27 15:34:03 +0530

    neeraj2

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/VouchersConsider/Columns.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/VouchersConsider/Columns.js
@@ -6,12 +6,14 @@ let CommonDuplicate = require("./Columns/Duplicate");
 let CommonDisplayToggles = require("./Columns/DisplayToggles");
 let CommonDefaultValue = require("./Columns/DefaultValue");
 let CommonDisplayColumnDelete = require("./Columns/DisplayColumnDelete");
+let CommonWidths = require("./Columns/Widths");
 
 router.use("/DisplayColumn", CommonDisplayColumn);
 router.use("/Duplicate", CommonDuplicate);
 router.use("/DisplayToggles", CommonDisplayToggles);
 router.use("/DefaultValue", CommonDefaultValue);
 router.use("/DisplayColumnDelete", CommonDisplayColumnDelete);
+router.use("/Widths", CommonWidths);
 
 
 module.exports = router;
