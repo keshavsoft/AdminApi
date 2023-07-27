beb9829061c12b977a7814530a0ceec0bdb03765
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-07-27 20:01:28 +0530

    SREENIVAS

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/VouchersConsider/Columns.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/VouchersConsider/Columns.js
@@ -6,14 +6,12 @@ let CommonDuplicate = require("./Columns/Duplicate");
 let CommonDisplayToggles = require("./Columns/DisplayToggles");
 let CommonDefaultValue = require("./Columns/DefaultValue");
 let CommonDisplayColumnDelete = require("./Columns/DisplayColumnDelete");
-let CommonWidths = require("./Columns/Widths");
 
 router.use("/DisplayColumn", CommonDisplayColumn);
 router.use("/Duplicate", CommonDuplicate);
 router.use("/DisplayToggles", CommonDisplayToggles);
 router.use("/DefaultValue", CommonDefaultValue);
 router.use("/DisplayColumnDelete", CommonDisplayColumnDelete);
-router.use("/Widths", CommonWidths);
 
 
 module.exports = router;
