99f3438271e0bca3e2ca91074276324fb5a6001e
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-08-10 19:03:47 +0530

    keshav

--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/AdminFolder/PreloadJsonFile/FromKeys/ConfigKey/JsonConfigKey.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/AdminFolder/PreloadJsonFile/FromKeys/ConfigKey/JsonConfigKey.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 let CommonControllers = require("../../../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/AdminFolder/PreloadJsonFile/FromKeys/ConfigKey/JsonConfigKey");
-let CommonMiddleWares = require("../../../../../../../../../MiddleWares/AdminApi/AsTree/Json/UserFolders/AdminFolder/PreloadJsonFile/FromKeys/ConfigKey/JsonConfigKey");
+let CommonMiddleWares = require("../../../../../../../../../Middlewares/AdminApi/AsTree/Json/UserFolders/AdminFolder/PreloadJsonFile/FromKeys/ConfigKey/JsonConfigKey");
 
 router.post('/',CommonMiddleWares.PostFunc, CommonControllers.PostFunc);
 router.delete('/', CommonMiddleWares.DeleteFunc, CommonControllers.DeleteFunc);
--- Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/DisplayNameAlter.js
+++ Projects/JSONAdminApi/Routes/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/DisplayNameAlter.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 let CommonControllers = require("../../../../../../../Controllers/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/DisplayNameAlter.Controllers");
-let CommonMiddleWares = require("../../../../../../../MiddleWares/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/DisplayNameAlter")
+let CommonMiddleWares = require("../../../../../../../Middlewares/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/DisplayNameAlter")
 
 router.get('/', CommonMiddleWares.GetFuncs, CommonControllers.GetFuncs);
 router.patch('/', CommonMiddleWares.PatchFuncs, CommonControllers.PatchFuncs);
