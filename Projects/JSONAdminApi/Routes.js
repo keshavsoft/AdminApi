fe7ad6e79364d4047da8b09be6e19c0072fc2ecc
Keshav <nknnkumar@yahoo.com>
2023-08-29 11:21:08 +0530

    keshav

--- Projects/JSONAdminApi/Routes.js
+++ Projects/JSONAdminApi/Routes.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
-let CommonMiddlewareJwtVerify = require("../../common/Jwt/Bs5");
+// let CommonMiddlewareJwtVerify = require("../../common/Jwt/Bs5");
 let CommonAdminApi = require("./Routes/AdminApi");
 let CommonValidate = require('./Routes/Validate');
+
+let CommonMiddlewareJwtVerify = require("../../common/Jwt/ForLogin/UserCredentials");
 
 router.use('/AdminApi', CommonMiddlewareJwtVerify.ForKeshavSoftRedirectToLogin, CommonAdminApi);
 router.use('/Validate', CommonValidate);
