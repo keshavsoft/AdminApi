cd8a6e189c673694f5b03672930675235a2caee0
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-09-20 20:21:46 +0530

    SREEENIVAS

---
+++ Projects/JSONAdminApi/Controllers/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/SubKeys/ServerSide/DefaultShowData.Controllers.js
@@ -0,0 +1,35 @@
+let Repos = require("../../../../../../../../../Repository/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/TableColumns/SubKeys/ServerSide/DefaultShowData");
+
+let GetFuncs = async (req, res) => {
+    let LocalDataPk = req.KeshavSoft.DataPk;
+
+    let LocalFromRepo = await Repos.GetFuncs({
+        DataPK: LocalDataPk
+    });
+
+    res.json(LocalFromRepo);
+};
+
+let Update = async (req, res) => {
+    let LocalDataPk = req.KeshavSoft.DataPk;
+
+    let jVarLocalfolderName = req.body.folderName;
+    let jVarLocalFileName = req.body.FileName;
+    let jVarLocalItemName = req.body.ItemName;
+    let jVarLocalScreenName = req.body.ScreenName;
+    let jVarLocalDataAttribute = req.body.DataAttribute;
+    let localUpdateBodyAsJson = req.body.BodyAsJson;
+
+    let LocalFromRepo = await Repos.Update({
+        DataPK: LocalDataPk,
+        folderName: jVarLocalfolderName,
+        FileName: jVarLocalFileName,
+        ItemName: jVarLocalItemName,
+        ScreenName: jVarLocalScreenName,
+        DataAttribute: jVarLocalDataAttribute,
+        BodyAsJson: localUpdateBodyAsJson
+    });
+    res.end(JSON.stringify(LocalFromRepo));
+
+};
+module.exports = { GetFuncs, Update };
