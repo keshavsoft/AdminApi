2f3a2cbe06af910509e066a96c2fd38e43813464
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-10-27 20:16:16 +0530

    SREENIVAS

---
+++ Projects/JSONAdminApi/Dal/AdminApi/AsTree/Json/UserFolders/ScreensFromDisplayJson/SubTableColumns/SubKeys/ServerSide/DefaultShowData.js
@@ -0,0 +1,21 @@
+let CommonDataSupplyForGet = require("../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/AsTree/ForSubTableColumns/SubMenu/ServerSide/DefaultShowData");
+ let CommonDataSupplyForUpdate = require("../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/ItemName/ScreenName/SubTableColumns/SubMenu/ServerSide/DefaultShowData/Update");
+
+ let GetFuncs = async ({ DataPK }) => {
+    if (DataPK > 0) {
+        return await CommonDataSupplyForGet.AsObject({ inDataPK: DataPK });
+    };
+};
+
+let Update = async ({ DataPK, FolderName, FileName, ItemName, ScreenName, subtablecolumnkey, DataAttribute, BodyAsJson }) => {
+    let LocalDataPk = DataPK;
+
+    if (LocalDataPk > 0) {
+        return await CommonDataSupplyForUpdate.Update({ DataPK, FolderName, FileName, ItemName, ScreenName, subtablecolumnkey, DataAttribute, BodyAsJson })
+    };
+};
+
+
+module.exports = {
+    GetFuncs, Update
+};
